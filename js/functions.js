48d187dda16dc079cef4ae1f342b52386c39eae0
HurricaneLily <jostin27022023@gmail.com>
2024-02-15 21:21:42 +0400

    Исправления

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,21 @@
-let isShort = (string, stringLength) => string.length <= stringLength;
+const isShort = (string, stringLength) => string.length <= stringLength;
 
 function isPalindrome (string) {
-  let normalizedString = string.toLowerCase().replaceAll(' ', '');
+  const normalizedString = string.toLowerCase().replaceAll(' ', '');
   let reversedString = '';
   for (let i = normalizedString.length - 1; i >= 0; i--) {
     reversedString += normalizedString[i];
   }
-  return normalizedString == reversedString;
+  return normalizedString === reversedString;
 }
 
 function findNumbers (string) {
   string = string.toString();
   let result = '';
   for (let i = 0; i < string.length; i++) {
-    number = parseInt(string[i]);
-    if (!Number.isNaN(number)) {
+    if (!Number.isNaN(parseInt(string[i], 10))) {
       result += string[i];
     }
   }
-  return parseInt(result);
+  return parseInt(result, 10);
 }
